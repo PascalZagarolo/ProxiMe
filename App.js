16c79563624e29ec091a049113694a9d3fa0765a
Pascal <zagarolopascal@gmail.com>
2024-02-05 14:29:40 +0100

    ...

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [counter, setCounter] = useState(0);
+
   return (
     <View style={styles.container}>
       <Text className="text-gray-900/80 ">
-        Willkommen bei <Text className="font-bold text-lg text-gray-900"> Proximity </Text>
-        
+       Willkommen bei <Text className="font-bold text-lg text-gray-900"> Proximity </Text>        
         </Text>
       <StatusBar style="auto" />
+      <View className="mt-2 bg-blue-800">
+      <Button
+      color="black"
+      
+      title='Direkt anfangen'
+      onPress={() => {setCounter(counter + 1)}}
+      />
+      </View>
+      
+        
+      
+      
     </View>
   );
 }
@@ -19,5 +34,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+  }, button : {
+    backgroundColor : 'black',
+    borderRadius: 10,
+  }
+})
+;
