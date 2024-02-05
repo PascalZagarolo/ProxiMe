755ee613fee7c5947007c09f5c1cb897cc1f8e4f
Pascal <zagarolopascal@gmail.com>
2024-02-05 13:39:21 +0100

    first text

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className="text-gray-900/80 ">
+        Willkommen bei <Text className="font-bold text-lg text-gray-900"> Proximity </Text>
+        
+        </Text>
       <StatusBar style="auto" />
     </View>
   );
